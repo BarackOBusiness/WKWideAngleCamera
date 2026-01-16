4f6d5c6dd84323e5fa80bcc1cb7dacd0d3cf05bd
BarackOBusiness <BarackOBusiness@proton.me>
2026-01-16 14:37:51 -0500

    Initial project

---
+++ src/Main.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace HighFOV;
+
+[BepInPlugin("wk.barackobusiness.stereographic", "High FOV Camera", "0.1.0")]
+public class HighFOVPlugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+        
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Wide angle views are now possible.");
+    }
+}
