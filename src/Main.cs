6670c6fe094cc45a51da1680271e9352e0096bec
BarackOBusiness <BarackOBusiness@proton.me>
2026-01-17 02:46:58 -0500

    Render cubemap and export as equirectangular

--- src/Main.cs
+++ src/Main.cs
@@ -5,15 +5,27 @@ using UnityEngine.SceneManagement;
 
 namespace HighFOV;
 
-[BepInPlugin("wk.barackobusiness.stereographic", "High FOV Camera", "0.1.0")]
-public class HighFOVPlugin : BaseUnityPlugin
+[BepInPlugin("wk.barackobusiness.wideangle", "High FOV Camera", "0.1.0")]
+public class WideAnglePlugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
-        
-    private void Awake()
-    {
+
+    internal const int cubemapSize = 512;
+
+    private void Awake() {
         // Plugin startup logic
         Logger = base.Logger;
         Logger.LogInfo($"Wide angle views are now possible.");
+
+        SceneManager.sceneLoaded += OnSceneLoad;
+    }
+
+    private void OnSceneLoad(Scene scene, LoadSceneMode mode) {
+        if (scene.name == "Game-Main") {
+            Transform camParent = GameObject.Find("CL_Player/Main Cam Root/Main Camera Shake Root").transform;
+            Transform camManager = new GameObject("Stereographic Camera").transform;
+            camManager.SetParent(camParent, false);
+            camManager.gameObject.AddComponent<StereographicCameraManager>();
+        }
     }
 }
