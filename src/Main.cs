7af1e6573841bc2fdfe7ec58499969bee533802b
BarackOBusiness <BarackOBusiness@proton.me>
2026-01-20 18:03:36 -0500

    Bundle assets and mirror the setup I had in unity
    as of now this is 90% working, there is one major problem:
    - using terminals breaks ui
    
    Other niceties might include:
    - setting field of view with the ingame slider
    - fixing the directional views to not fog at such a low distance

--- src/Main.cs
+++ src/Main.cs
@@ -1,5 +1,8 @@
-﻿using BepInEx;
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
 using BepInEx.Logging;
+using BepInEx.Configuration;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,22 +13,74 @@ public class WideAnglePlugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
 
-    internal const int cubemapSize = 512;
+    private ConfigEntry<int> resolution;
+    private ConfigEntry<float> fieldOfView;
+
+    private Material stereoMat;
+    private GameObject projector;
+    private GameObject stereoCam;
 
     private void Awake() {
-        // Plugin startup logic
         Logger = base.Logger;
-        Logger.LogInfo($"Wide angle views are now possible.");
 
-        SceneManager.sceneLoaded += OnSceneLoad;
+        resolution = Config.Bind(
+            "General", "Resolution", 512,
+            "The default side length of a face on the cubemap"
+        );
+        fieldOfView = Config.Bind(
+            "General", "Field of view", 170.0f,
+            "The field of view of the larger axis of your display, for pretty much everybody this will be horizontal field of view"
+        );
+
+        bool bundleStatus = LoadBundleAssets();
+
+        if (bundleStatus) {
+            SceneManager.sceneLoaded += OnSceneLoad;
+            Logger.LogInfo("Wide angle views are now possible.");
+        }
     }
 
     private void OnSceneLoad(Scene scene, LoadSceneMode mode) {
-        if (scene.name == "Game-Main") {
-            Transform camParent = GameObject.Find("CL_Player/Main Cam Root/Main Camera Shake Root").transform;
-            Transform camManager = new GameObject("Stereographic Camera").transform;
-            camManager.SetParent(camParent, false);
-            camManager.gameObject.AddComponent<StereographicCameraManager>();
+        if (scene.name == "Game-Main" || scene.name == "Playground") {
+            Transform camParent = Camera.main.transform;
+            GameObject camManager = Instantiate(stereoCam, camParent);
+            GameObject projectorScreen = Instantiate(projector, camParent);
+            projectorScreen.layer = 31;
+            projectorScreen.transform.localPosition = new Vector3(0f, 0f, 0.5f);
+            projectorScreen.transform.localRotation = Quaternion.Euler(0f, 0f, 180f);
+            projectorScreen.transform.localScale = new Vector3(Camera.main.aspect, 1f, 1f);
+            camManager.AddComponent<StereographicCameraManager>().Init(
+                projectorScreen.GetComponent<MeshRenderer>(), resolution.Value, fieldOfView.Value
+            );
+            Camera.main.orthographic = true;
+            Camera.main.orthographicSize = 0.5f;
+            Camera.main.cullingMask = 1 << 31;
         }
+    }
+
+    private bool LoadBundleAssets() {
+        var dllDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var path = Path.Combine(dllDir, "stereoassets");
+        var bundle = AssetBundle.LoadFromFile(path);
+
+        if (bundle == null) {
+            Logger.LogError("Wide angle views are NOT possible, please check if the asset bundle is in the same directory as the plugin.");
+            return false;
+        }
+
+        stereoMat = bundle.LoadAsset<Material>("Screen");
+        projector = bundle.LoadAsset<GameObject>("Projector");
+        stereoCam = bundle.LoadAsset<GameObject>("Stereographic Camera");
+
+        if (stereoMat == null || projector == null || stereoCam == null) {
+            Logger.LogError("Wide angle views are NOT possible, please check the checksum of the asset bundle in the plugin directory, if it does not match X, reacquire this bundle");
+            return false;
+        }
+
+        DontDestroyOnLoad(stereoMat);
+        DontDestroyOnLoad(projector);
+        DontDestroyOnLoad(stereoCam);
+
+        return true;
     }
 }
--- src/StereographicCameraManager.cs
+++ src/StereographicCameraManager.cs
@@ -1,82 +1,61 @@
-using System.IO;
 using UnityEngine;
 
 namespace HighFOV;
 
 public class StereographicCameraManager : MonoBehaviour {
-	internal Camera Forward;
-	internal Camera Back;
-	internal Camera Left;
-	internal Camera Right;
-	internal Camera Bottom;
-	internal Camera Top;
+	// Perspective as in where the player actually sees from
+	private Camera perspective;
+	private Camera front;
+	private Camera left;
+	private Camera right;
+	private Camera down;
+	private Camera up;
 
-	internal RenderTexture cubeRT;
-	internal RenderTexture equirectRT;
-
-	private void Awake() {
-		// Setup the cubemap for copying into
-		cubeRT = new RenderTexture(WideAnglePlugin.cubemapSize, WideAnglePlugin.cubemapSize, 16);
-		cubeRT.dimension = UnityEngine.Rendering.TextureDimension.Cube;
-		// Setup the equirect for previews
-		equirectRT = new RenderTexture(cubeRT.width * 2, cubeRT.height, 16);
+	private RenderTexture cubemap;
 	
-		Forward = BuildCamera("Forward", transform);
-		Forward.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
-		Back = BuildCamera("Back", transform);
-		Back.transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
-		Left = BuildCamera("Left", transform);
-		Left.transform.localRotation = Quaternion.Euler(0f, 270f, 0f);
-		Right = BuildCamera("Right", transform);
-		Right.transform.localRotation = Quaternion.Euler(0f, 90f, 0f);
-		Bottom = BuildCamera("Bottom", transform);
-		Bottom.transform.localRotation = Quaternion.Euler(270f, 0f, 0f);
-		Top = BuildCamera("Top", transform);
-		Top.transform.localRotation = Quaternion.Euler(90f, 0f, 0f);
+	public void Init(Renderer projector, int size, float fov) {
+		perspective = Camera.main;
+		front = transform.GetChild(0).GetComponent<Camera>();
+		SetupCam(front, size);
+		left = transform.GetChild(1).GetComponent<Camera>();
+		SetupCam(left, size);
+		right = transform.GetChild(2).GetComponent<Camera>();
+		SetupCam(right, size);
+		down = transform.GetChild(3).GetComponent<Camera>();
+		SetupCam(down, size);
+		up = transform.GetChild(4).GetComponent<Camera>();
+		SetupCam(up, size);
 
-		// Camera stereoCam = gameObject.AddComponent<Camera>();
-		// stereoCam.clearFlags = CameraClearFlags.SolidColor;
-		// stereoCam.backgroundColor = Color.black;
-		// stereoCam.orthographic = true;
-		// stereoCam.orthographicSize = 0.5f;
-		// stereoCam.cullingMask = LayerMask.GetMask()
+		cubemap = new RenderTexture(size, size, 16);
+		cubemap.dimension = UnityEngine.Rendering.TextureDimension.Cube;
+
+		// Assign properties to the shader
+		projector.material.SetTexture("_Cube", cubemap);
+		projector.material.SetFloat("_FOV", fov);
 	}
 
-	private Camera BuildCamera(string name, Transform parent) {
-		GameObject camObject = new GameObject(name);
-		camObject.transform.SetParent(parent, false);
-		Camera cam = camObject.AddComponent<Camera>();
-		int resolution = WideAnglePlugin.cubemapSize;
-		cam.targetTexture = new RenderTexture(resolution, resolution, 16);
-		cam.backgroundColor = Color.black;
-		cam.fieldOfView = 90.0f;
-		cam.aspect = 1.0f;
-		return cam;
+	private void Update() {
+		// Populate the cubemap with the newly rendered views
+		Graphics.CopyTexture(front.targetTexture, 0, 0, cubemap, 4, 0);
+		Graphics.CopyTexture(left.targetTexture, 0, 0, cubemap, 1, 0);
+		Graphics.CopyTexture(right.targetTexture, 0, 0, cubemap, 0, 0);
+		Graphics.CopyTexture(up.targetTexture, 0, 0, cubemap, 3, 0);
+		Graphics.CopyTexture(down.targetTexture, 0, 0, cubemap, 2, 0);
 	}
 
-    public void ExportCubemap() {
-        // First we have to render the cameras to the cubemap
-        Graphics.CopyTexture(Forward.targetTexture, 0, 0, cubeRT, 4, 0);
-        Graphics.CopyTexture(Back.targetTexture, 0, 0, cubeRT, 5, 0);
-        Graphics.CopyTexture(Left.targetTexture, 0, 0, cubeRT, 1, 0);
-        Graphics.CopyTexture(Right.targetTexture, 0, 0, cubeRT, 0, 0);
-        Graphics.CopyTexture(Bottom.targetTexture, 0, 0, cubeRT, 3, 0);
-        Graphics.CopyTexture(Top.targetTexture, 0, 0, cubeRT, 2, 0);
+	private void OnDestroy() {
+		cubemap.Release();
+		front.targetTexture.Release();
+		left.targetTexture.Release();
+		right.targetTexture.Release();
+		down.targetTexture.Release();
+		up.targetTexture.Release();
+	}
 
-        RenderTexture previousMain = RenderTexture.active;
-
-        cubeRT.ConvertToEquirect(equirectRT);
-        RenderTexture.active = equirectRT;
-
-        int width = equirectRT.width, height = equirectRT.height;
-        Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, 0, true);
-        tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
-        tex.Apply();
-
-        byte[] imageData = tex.EncodeToPNG();
-        File.WriteAllBytes(Application.dataPath + "/cubemap.png", imageData);
-
-        Destroy(tex);
-        RenderTexture.active = previousMain;
-    }
+	private void SetupCam(Camera cam, int size) {
+		var RT = new RenderTexture(size, size, 16);
+		cam.targetTexture = RT;
+		cam.cullingMask = perspective.cullingMask;
+		cam.clearFlags = perspective.clearFlags;
+	}
 }
